2715c0d1725ede440e4198361d7e4594a6752cfe
Artem Karpov <komhleb@yandex.ru>
2019-10-13 17:39:20 +0300

    fix naming

---
+++ ApiHelper.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+
+class ApiHelper:
+
+    def post_api(self, data, header):
+        url = "https://gorest.co.in/public-api/users"
+        response = requests.post(url, headers=header, json=data)
+
+        global user_id
+        user_id = json.dumps(response.json()['result']['id']).replace('"', '').rstrip('\n')
+
+        response_code = json.dumps(response.json()['_meta']['code'])
+        print(f'\nID #{user_id} POST with response {response_code}')
+
+        return response_code
+
+    def get_api(self):
+        cont_type = 'json'
+        token = 'TBi_Y27sxJ9XsBBmRgkb-oXSpjbWUdsotju1'
+        url = f'https://gorest.co.in/public-api/users/{user_id}?_format={cont_type}&access-token={token}'
+        response = requests.get(url)
+
+        response_code = json.dumps(response.json()['_meta']['code'])
+        print(f'\nID #{user_id} GET with response {response_code}')
+
+        return response_code
+
+    def delete_api(self, header):
+        url = f'https://gorest.co.in/public-api/users/{user_id}'
+        response = requests.delete(url, headers=header)
+
+        response_code = json.dumps(response.json()['_meta']['code'])
+        print(f'\nID #{user_id} DELETE with response {response_code}')
+
+        return response_code
--- TestApiExecutor.py
+++ TestApiExecutor.py
@@ -1,6 +1,6 @@
 import pytest
 import json
-from TestApi import *
+from ApiHelper import *
 
 
 def read_file(path):
@@ -9,12 +9,12 @@ def read_file(path):
 
 
 def test_case():
-    test1 = TestApi()
+    test1 = ApiHelper()
     try:
         print(f'\n-- Test POST-GET-DELETE-GET started --')
-        assert test1.post_api(read_file('data.txt'), read_file('header.txt')) == '200'
+        assert test1.post_api(read_file('data.json'), read_file('header.json')) == '200'
         assert test1.get_api() == '200'
-        assert test1.delete_api(read_file('header.txt')) == '204'
+        assert test1.delete_api(read_file('header.json')) == '204'
         assert test1.get_api() == '404'
 
         print(f'\n-- Test POST-GET-DELETE-GET finished --')
