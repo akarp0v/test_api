0c0197df085d1317a1c778535326d2af4a67642b
owl <komhleb@yandex.ru>
2020-03-25 15:59:56 +0300

    test 2 added

--- TestApiExecutor.py
+++ TestApiExecutor.py
@@ -1,22 +1,36 @@
-import pytest
 import json
-from ApiHelper import *
+from ApiHelper import ApiHelper
 
 
 def read_file(path):
-    with open(path, 'r') as file:
+    with open(path) as file:
         return json.loads(file.read())
 
 
-def test_case():
+def test_1():
     test1 = ApiHelper()
     try:
-        print(f'\n-- Test POST-GET-DELETE-GET started --')
+        print('\n-- Test POST-GET-DELETE-GET started --\n')
         assert test1.post_api(read_file('header.json')) == '200'
         assert test1.get_api() == '200'
         assert test1.delete_api(read_file('header.json')) == '204'
         assert test1.get_api() == '404'
+        print('\n-- Test POST-GET-DELETE-GET finished --\n')
 
-        print(f'\n-- Test POST-GET-DELETE-GET finished --')
     except OSError:
-        print(f'\n-- Test POST-GET-DELETE-GET was NOT finished --')
+        print('\n-- Test POST-GET-DELETE-GET failed --\n')
+
+
+def test_2():
+    test2 = ApiHelper()
+    try:
+        print('\n-- Test POST-PUT-GET-DELETE-GET started --\n')
+        assert test2.post_api(read_file('header.json')) == '200'
+        assert test2.put_api() == '200'
+        assert test2.get_api() == '200'
+        assert test2.delete_api(read_file('header.json')) == '204'
+        assert test2.get_api() == '404'
+        print('\n-- Test POST-PUT-GET-DELETE-GET finished --\n')
+
+    except OSError:
+        print('\n-- Test POST-PUT-GET-DELETE-GET failed --\n')
