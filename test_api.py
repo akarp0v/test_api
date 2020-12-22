9f6abb20eb37fe54407a93b88ced6becb2d13efa
akarp0v <komhleb@yandex.ru>
2020-12-22 14:39:40 +0300

    code refactored

--- test_api.py
+++ test_api.py
@@ -3,22 +3,18 @@ from ApiHelper import ApiHelper
 
 def test_post_get_delete():
     api = ApiHelper()
-    try:
-        assert api.post_api() == 201
-        assert api.get_api() == 200
-        assert api.delete_api() == 204
-        assert api.get_api() == 404
-    except OSError:
-        print('Test FAILED')
+    assert api.post_api() == 201
+    assert api.get_api() == 200
+    assert api.delete_api() == 204
+
+    assert api.get_api() == 404
 
 
 def test_post_put_get_delete():
     api = ApiHelper()
-    try:
-        assert api.post_api() == 201
-        assert api.put_api() == 200
-        assert api.get_api() == 200
-        assert api.delete_api() == 204
-        assert api.get_api() == 404
-    except OSError:
-        print('Test FAILED')
+    assert api.post_api() == 201
+    assert api.put_api() == 200
+    assert api.get_api() == 200
+    assert api.delete_api() == 204
+
+    assert api.get_api() == 404
